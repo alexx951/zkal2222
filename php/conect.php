e5418e3812c080dad80a8ad43b388cba6a54c11f
alexx951 <136070677+alexx951@users.noreply.github.com>
2023-06-26 10:06:39 +0200

    uidnguezn

--- php/conect.php
+++ php/conect.php
@@ -3,41 +3,36 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-         if (isset($_post["bo"]))  {
+if (isset($_post["bo"])) 
+  {
         
 $u=$_post["user"];
 $p=$_post["password"];
-
-
-
-     }
+ }
+                           
+$username = $_POST['username'];
+$password = $_POST['password'];
+$link = new mysqli("localhost", "root", "", "ipssi_quizzeo");
+// if ($link -> connect_errno >0)
+//  {
+// echo"failed to connect to sql" . mysqli_connect_errno();
+// exit;
+//  }
+$sql = "SELECT * FROM usr_user WHERE usr_pseudo ='$username' and usr_password = '$password'" ; 
+$result = $link->query($sql);
    
-    $username = $_POST['username'];
-    
-    $password = $_POST['password'];
-    $link = new mysqli("localhost", "root", "", "ipssi_quizzeo");
-   
-
-
-    $sql = "SELECT * FROM usr_user WHERE usr_pseudo ='$username' and usr_password = '$password'" ; 
-    $result = $link->query($sql);
-   
-     if
-     (mysqli_num_rows($result)>0){
-        
-        echo "ça marche"; 
-    header('location: ../html/listequizz.php');
-    return true;
-      }
+if(mysqli_num_rows($result)>0)
+ { 
+header('location: ../html/listequizz.php');
+return true;
+ }
          
-    else {
-              
-             
-    
-    echo"mauvais pseudo ou mot de passe ";
-    return false;
-          }
+else 
+ {
+echo"mauvais pseudo ou mot de passe ";
+return false;
+ }
       
-        }
+}
 
 ?>
